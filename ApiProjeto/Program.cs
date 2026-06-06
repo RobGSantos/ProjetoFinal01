7fb63b4eed160e9cdd15a26ac9c0b2ecf93502c5
Robson Gonçalves Santos <robsongsantos@PREFEITURA.SP.GOV.BR>
2026-06-06 20:39:32 -0300

    feat: configure ef core sqlite e identity

--- ApiProjeto/Program.cs
+++ ApiProjeto/Program.cs
@@ -1,6 +1,33 @@
+using ApiProjeto.Data;
+using ApiProjeto.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' não foi localizado");
+
+
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlite(connectionString));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+    {
+        options.Password.RequireDigit = true;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireNonAlphanumeric = false;
+
+        options.User.RequireUniqueEmail = true; 
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,6 +45,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
